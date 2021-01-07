0339614f0219d539d7cfcd70ba2a806adaca10e0
Mahdi Sheikh Hosseini <mah454@gmail.com>
2021-01-07 23:02:02 +0330

    Improvement installation script

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash 
 
+if [ $UID -ne 0 ] ; then 
+		echo "Please execute command as root"
+		exit 1
+fi
+
 INSTALL_PATH="/opt/aparat-dl" 
 
 if [ ! -d $INSTALL_PATH ] ; then 
 		mkdir $INSTALL_PATH 
 fi
 
-install -m 644 target/aparat-dl.jar /opt/aparat-dl/aparat-dl.jar
-install -m 755 aparat-dl /opt/aparat-dl/aparat-dl
-update-alternatives --install /usr/local/bin/aparat-dl aparat-dl /opt/aparat-dl/aparat-dl 100
+install -m 644 target/aparat-dl.jar $INSTALL_PATH/aparat-dl.jar
+install -m 755 aparat-dl $INSTALL_PATH/aparat-dl
+update-alternatives --install /usr/local/bin/aparat-dl aparat-dl $INSTALL_PATH/aparat-dl 100
 
 
